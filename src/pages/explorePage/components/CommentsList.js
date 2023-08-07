5e07628f544e8feef3ce2fbcae89fd98c4d96be9
Viktoriia <toriakozenko@gmail.com>
2023-08-07 15:07:35 +0300

    some chages

--- src/pages/explorePage/components/CommentsList.js
+++ src/pages/explorePage/components/CommentsList.js
@@ -5,10 +5,15 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Comment from './Comment';
 
 function CommentsList({comments}) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleAccordionClick = () => {
+    setIsExpanded(!isExpanded);
+  };
   return ( 
   
     comments && comments.length ? 
-    <Accordion sx={{ border: 'transparent'}}>
+    <Accordion sx={{ border: 'transparent'}} expanded={isExpanded} onChange={handleAccordionClick}>
       
     <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{  fontWeight: 'light', fontSize: 14 }}>
       View all comments
